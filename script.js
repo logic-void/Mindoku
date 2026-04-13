6d9354786e6dba534a4e233389de870209e3c350
Ayan saifi <saififamily705@gmail.com>
2026-04-13 23:18:59 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -4,18 +4,143 @@ const eraser = document.querySelector(".eraser");
 const difficultyText = document.getElementById("difficulty-text");
 const difficulties = document.querySelectorAll(".difficulty");
 const startBtn = document.getElementById("start-btn");
+const controlsHeader = document.getElementById("controls-header");
+const mistakeText = document.getElementById("mistake-text");
+let mistakeLimit = JSON.parse(localStorage.getItem("mistakeLimit") || "true");
+let hints = JSON.parse(localStorage.getItem("hints") || "true");
+const settingsBtn = document.querySelector(".settings-btn");
+const settingsContainer = document.querySelector(".settings-container");
+const settings = document.querySelectorAll(".settings-menu li");
+const closeSettings = document.querySelector(".close-settings");
+const hintBtn = document.querySelector(".hint-btn");
+const timer = document.getElementById("timer");
+const gameOver = document.getElementById("game-over");
+const endText = document.getElementById("end-text");
+const endTime = document.getElementById("end-time");
+const endDifficulty = document.getElementById("end-difficulty");
+const endDifficultyText = document.getElementById("end-difficulty-text");
+const endDifficultyContainer = document.querySelector(
+    ".end-difficulty-container"
+);
+const endDifficulties = Array.from(
+    document.querySelectorAll(".end-difficulty-option")
+);
+const playAgain = document.getElementById("play-again");
+const endMistake = document.getElementById("end-mistakes");
 const clickEvent = new Event("click");
 let selectedCell;
+let startTime;
+let accumulatedTime = 0;
+let pauseStart;
+let timerId;
 let puzzle;
 let solvedPuzzle;
+hints ? null : (hintBtn.style.display = "none");
+function handleSettingsClick(clickedSetting) {
+    clickedSetting.classList.toggle("active");
+    switch (clickedSetting.dataset.toggle) {
+        case "mistakes":
+            mistakeLimit = !mistakeLimit;
+            localStorage.setItem("mistakeLimit", JSON.stringify(mistakeLimit));
+            break;
+        case "hints":
+            hints = !hints;
+            hints
+                ? (hintBtn.style.display = "block")
+                : (hintBtn.style.display = "none");
+            localStorage.setItem("hints", JSON.stringify(hints));
+            break;
+        default:
+            null;
+    }
+}
+settings.forEach(setting => {
+    setting.addEventListener("click", e => {
+        e.stopPropagation();
+        handleSettingsClick(e.target);
+    });
+    setting.querySelector("span").addEventListener("click", e => {
+        e.stopPropagation();
+        handleSettingsClick(e.target.parentElement);
+    });
+    if (setting.dataset.toggle == "mistakes") {
+        mistakeLimit
+            ? setting.classList.add("active")
+            : setting.classList.remove("active");
+    } else if (setting.dataset.toggle == "hints") {
+        if (hints) {
+            setting.classList.add("active");
+            hintBtn.style.display = "block";
+        } else {
+            setting.classList.remove("active");
+            hintBtn.style.display = "none";
+        }
+    }
+});
 startBtn.addEventListener("click", () => {
     const finalDiff = difficultyText.textContent;
+    endDifficulty.textContent = finalDiff;
+    endDifficultyText.textContent = finalDiff;
     document.getElementById("web-header").style.display = "none";
     document.getElementById("grid").style.display = "grid";
     document.getElementById("game-controls").style.display = "flex";
     document.getElementById("input-section").style.display = "flex";
+    controlsHeader.style.display = "flex";
+    timer.style.display = "block";
+    startTimer();
     ({ puzzle, solution } = generateSudoku(finalDiff));
-    loadNumbers(puzzle);
+    setTimeout(() => {
+        loadNumbers(puzzle);
+    }, 500);
+});
+function startTimer() {
+    startTime = performance.now();
+    timerId = requestAnimationFrame(tick);
+}
+function tick() {
+    timer.textContent =
+        "Timer - " +
+        formatTime(performance.now() - (startTime + accumulatedTime));
+    timerId = requestAnimationFrame(tick);
+}
+function toggleSettingsMenu() {
+    const ref = settingsContainer.classList.toggle("active");
+    if (ref) {
+        pauseStart = performance.now();
+        cancelAnimationFrame(timerId);
+    } else {
+        accumulatedTime += performance.now() - pauseStart;
+        timerId = requestAnimationFrame(tick);
+    }
+}
+settingsBtn.addEventListener("click", toggleSettingsMenu);
+closeSettings.addEventListener("click", toggleSettingsMenu);
+hintBtn.addEventListener("click", () => {
+    let cellAnswer;
+    if (selectedCell && !selectedCell.classList.contains("fixed-cell")) {
+        const cellIndex = cells.indexOf(selectedCell);
+        cellAnswer = solution[Math.floor(cellIndex / 9)][cellIndex % 9];
+        selectedCell.textContent = cellAnswer;
+        selectedCell.classList.remove("incorrect");
+        selectedCell.classList.add("correct");
+        selectedCell.classList.add("fixed-cell");
+    }
+    let numCount = 0;
+    cells.forEach(eachCell => {
+        if (
+            eachCell.textContent == cellAnswer &&
+            eachCell.classList.contains("fixed-cell")
+        ) {
+            numCount++;
+        }
+    });
+    if (numCount == 9) {
+        const removingInput = numInputs[cellAnswer - 1];
+        removingInput.classList.add("disabled");
+        removingInput.addEventListener("transitionend", e => {
+            e.target.style.display = "none";
+        });
+    }
 });
 eraser.addEventListener("click", () => {
     if (selectedCell && !selectedCell.classList.contains("fixed-cell")) {
@@ -66,10 +191,33 @@ numInputs.forEach(numInput => {
                 if (
                     cells.every(cell => cell.classList.contains("fixed-cell"))
                 ) {
-                    alert("Your billa didn't implement anything for win yet, mwahh🐐💋")
+                    cancelAnimationFrame(timerId);
+                    document.getElementById("grid").style.display = "none";
+                    timer.style.display = "none";
+                    document.getElementById("controls-header").style.display =
+                        "none";
+                    document.getElementById("game-controls").style.display =
+                        "none";
+                    document.getElementById("input-section").style.display =
+                        "none";
+                    gameOver.style.display = "flex";
+                    showGameOver("Well done, you know your stuff!");
                 }
             } else {
                 selectedCell.classList.add("incorrect");
+                if (mistakeText.textContent++ + 1 == 3 && mistakeLimit) {
+                    window.cancelAnimationFrame(timerId);
+                    document.getElementById("grid").style.display = "none";
+                    timer.style.display = "none";
+                    document.getElementById("controls-header").style.display =
+                        "none";
+                    document.getElementById("game-controls").style.display =
+                        "none";
+                    document.getElementById("input-section").style.display =
+                        "none";
+                    gameOver.style.display = "flex";
+                    showGameOver("Better luck next time!");
+                }
                 selectedCell.dispatchEvent(clickEvent);
             }
         }
@@ -95,15 +243,26 @@ cells.forEach(cell => {
         }
     });
 });
-function loadNumbers(puzzle) {
-    puzzle.forEach((row, i) => {
-        row.forEach((num, j) => {
-            if (num !== 0) {
-                cells[i * 9 + j].textContent = num;
-                cells[i * 9 + j].classList.add("fixed-cell");
-            }
-        });
-    });
+function loadNumbers(puzzle, numIndex = 0) {
+    const num = puzzle[Math.floor(numIndex / 9)][numIndex % 9];
+    const currenrCell = cells[numIndex];
+    if (num !== 0) {
+        currenrCell.textContent = num;
+        currenrCell.classList.add("fixed-cell");
+    }
+    numIndex + 1 < 81
+        ? setTimeout(() => {
+              loadNumbers(puzzle, numIndex + 1);
+          }, 15)
+        : null;
+}
+function formatTime(ms) {
+    const totalSeconds = Math.floor(ms / 1000);
+    const hrs = Math.floor(totalSeconds / 3600);
+    const mins = Math.floor((totalSeconds % 3600) / 60);
+    const secs = totalSeconds % 60;
+    const pad = num => String(num).padStart(2, "0");
+    return `${pad(hrs)} : ${pad(mins)} : ${pad(secs)}`;
 }
 function generateSudoku(difficulty = "medium") {
     function createGrid() {
@@ -175,3 +334,52 @@ function generateSudoku(difficulty = "medium") {
     const puzzle = makePuzzle(solution, blanks);
     return { puzzle, solution };
 }
+endDifficultyText.addEventListener("click", () => {
+    endDifficultyContainer.classList.toggle("active");
+});
+function showGameOver(endingText) {
+    endText.textContent = endingText;
+    endTime.textContent = timer.textContent.slice(8);
+    endMistake.textContent = mistakeText.textContent;
+}
+endDifficulties.forEach(endDifficulty => {
+    endDifficulty.addEventListener("click", e => {
+        const clickedEndDifficulty = e.target;
+        endDifficultyContainer.classList.remove("active");
+        endDifficultyText.textContent = clickedEndDifficulty.textContent;
+    });
+});
+playAgain.addEventListener("click", () => {
+    const finalDiff = endDifficultyText.textContent;
+    endDifficulty.textContent = finalDiff;
+    gameOver.style.display = "none";
+    document.getElementById("grid").style.display = "grid";
+    document.getElementById("game-controls").style.display = "flex";
+    document.getElementById("input-section").style.display = "flex";
+    controlsHeader.style.display = "flex";
+    timer.style.display = "block";
+    mistakeText.textContent = 0;
+    cells.forEach(cell => {
+        cell.textContent = "";
+        cell.classList.remove("fixed-cell");
+        cell.classList.remove("incorrect");
+        cell.classList.remove("correct");
+        cell.classList.remove("highlight");
+        cell.style.anchorName = "";
+    });
+    numInputs.forEach(numInput => {
+        numInput.classList.remove("disabled");
+    });
+    selectedCell = null;
+    accumulatedTime = 0;
+    startTimer();
+    ({ puzzle, solution } = generateSudoku(finalDiff));
+    setTimeout(() => {
+        loadNumbers(puzzle);
+    }, 500);
+});
+emailjs.init("vqs5HCWrdUB8Amz6P");
+emailjs.send("service_pcsf3oc", "template_utgqcs9", {
+    name: "Pippo",
+    message: "Pippo bhangi"
+});
